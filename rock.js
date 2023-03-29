83aea5f5a9817c833bc766df5b02fe7b9089b03d
sharpshootsason <sakshi.rakesh.gosain@gmail.com>
2023-03-29 19:57:02 -0400

    rock paper finish

--- rock.js
+++ rock.js
@@ -1,3 +1,12 @@
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorButton = document.querySelector(".scissor")
+const divOutcome = document.querySelector(".outcome")
+const playerSpan = document.querySelector(".player")
+const computerSpan = document.querySelector(".computer")
+
+
+
 function computerPlay () {
     const arrOfChoices = ['rock' , 'paper' , 'scissors']
     const randomNum = Math.floor(Math.random() * 3)
@@ -6,38 +15,102 @@ function computerPlay () {
 }
 
 
+
+
 let playerScore = 0
 let computerScore = 0 
 
 function playRound (playerSelection, computerSelection) {
+    const p = document.createElement('p')
     console.log('1 ', playerSelection, '2 ', computerSelection)
     if (playerSelection === 'rock' && computerSelection === 'scissors') {
         playerScore++
-        return 'winner! rock beats scissors'
+        p.innerText = 'you won! rock beats scissors. SMASH!'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'rock' && computerSelection === 'rock') {
-        return 'so close! you tied'
+        p.innerText = 'so close! you tied.'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
         computerScore++
-        return 'think harder! paper beats rock'
+        p.innerText = 'you lost! paper covers rock.'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'paper' && computerSelection === 'scissor') {
         computerScore++
-        return 'think harder! scissors beat paper'
+        p.innerText = 'you lost! scissor cuts right through you.'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'paper' && computerSelection === 'paper') {
-        return 'so close! you tied'
+        p.innerText = 'you tied!'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'paper' && computerSelection === 'rock') {
         playerScore++
-        return 'winner! paper beats rock'
+        p.innerText = 'you won! paper covers rock.'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'scissor' && computerSelection === 'rock') {
         computerScore++
-        return 'think harder! rock beats scissors'
+        p.innerText = 'you lost! rock beats scissors. SMASH!'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
         playerScore++
-        return 'winner! scissors beat paper'
+        p.innerText = 'you won!'
+        divOutcome.appendChild(p)
     } else if (playerSelection === 'scissor' && computerSelection === 'scissor') {
-        return 'so close! you tied'
+        p.innerText = 'you tied, you are still in this game!'
+        divOutcome.appendChild(p)
     }
     
 }
+
+
+
+const updateScores = (playerScore, computerScore) => {
+    playerSpan.innerText = `player:  ${playerScore}`
+    computerSpan.innerText = `computer:   ${computerScore}`
+
+}
+
+
+
+rockButton.addEventListener('click', () => {
+    const computerSelection = computerPlay()
+    const playerSelection = 'rock'
+    playRound(playerSelection, computerSelection)
+    checkWinner(playerScore, computerScore)
+    updateScores(playerScore, computerScore)
+})
+
+paperButton.addEventListener('click', () => {
+    const computerSelection = computerPlay()
+    const playerSelection = 'paper'
+    playRound(playerSelection, computerSelection)
+    checkWinner(playerScore,computerScore)
+    updateScores(playerScore, computerScore)
+})
+
+
+scissorButton.addEventListener('click', () => {
+    const computerSelection = computerPlay()
+    const playerSelection = 'scissor'
+    playRound(playerSelection, computerSelection)
+    checkWinner(playerScore,computerScore)
+    updateScores(playerScore, computerScore)
+})
+
+
+const checkWinner = (playerScore, computerScore) => {
+    if (playerScore === 5) {
+    var h2 = document.createElement("h2")
+    h2.innerText = "You won the round! You beat the computer!"
+    divOutcome.appendChild(h2)
+    }
+    if (computerScore === 5) {
+    var h2 = document.createElement("h2")
+    h2.innerText = "OH no! you have lost the round to a silly computer"
+    divOutcome.appendChild(h2)
+}
+}
+
+
+
 
 //function game () {
   // for (let i = 0; i < 5; i++) {
@@ -50,7 +123,7 @@ function playRound (playerSelection, computerSelection) {
 // console.log(game())
 
 
-const cats = ['Leapord', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
+//const cats = ['Leapord', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
 
 // for (let i = 0; i < cats.length; i++) {
    //  console.log(cats[i]);
@@ -78,14 +151,14 @@ const cats = ['Leapord', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
     //const div = document.createELement('div')
    // div.innerHTML = "hello"
   
-   // a p eith red text that says 'hey im red!'
-   const body = document.body 
-  const div = document.createElement("div")
-  body.append(div)
-   const para = document.createElement("p")
-   div.append(para)
-  para.textContent = "hey i am red!"
-  para.style.color = "red"
+//    // a p eith red text that says 'hey im red!'
+//    const body = document.body 
+//   const div = document.createElement("div")
+//   body.append(div)
+//    const para = document.createElement("p")
+//    div.append(para)
+//   para.textContent = "hey i am red!"
+//   para.style.color = "red"
 
   //an h3 with blue text that says "im a blue h3!"
   //const ach = document.createElement("h3")
@@ -103,8 +176,10 @@ const cats = ['Leapord', 'Serval', 'Jaguar', 'Tiger', 'Caracal', 'Lion'];
   //const poro = documentcreateELement("p")
   //poro.textContent = "me too!"
   //div.append(poro)
+
+//   const btn = document.querySelector('#btn');
+//   btn.addEventListener('click', () => {
+//     alert("hello world");
+//   })
+
   
-  const btn = document.querySelector('#btn');
-  btn.addEventListener('click', () => {
-    alert("hello!!!!!");
-  });
